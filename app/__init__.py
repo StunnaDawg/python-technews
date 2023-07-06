3d096f9d44d0b8836735b6184451475e689a503b
StunnaDawg <jonsonallen9@gmail.com>
2023-07-06 16:13:22 -0300

    dashbaord/home view

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
-
-def create_app(test_config=None):
-    #sets up config (this is like index in react)
-    app = Flask(__name__, static_url_path='/')
-    app.url_map.strict_slashes = False
-    app.config.from_mapping(
-        secret = 'SHHH_SEcret_key'
+## imports home form routes folders index
+from app.routes import home
+from app.routes import dashboard
+#sets up config (this is like index in react)
+def create_app(test_config=None):    
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.config.from_mapping(
+    secret = 'SHHH_SEcret_key'
     )
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello World'
+#sets up routes
+  @app.route('/hello')
+  def hello():
+    return 'Hello World'
     
-    return app
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+
+  return app
